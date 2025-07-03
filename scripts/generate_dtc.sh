6a69ffbe2f226b58fe4d0f20e71db783d3e61806
Emptea <aryas1725@gmail.com>
2025-07-03 19:08:04 +0300

    upd generate_dtc to enable SD card from u-boot & merge all generated files to one .dts

--- scripts/generate_dtc.sh
+++ scripts/generate_dtc.sh
@@ -4,7 +4,7 @@
 
 PROJ_FILE=$1
 DT_REPO_PATH=$2
-OUTPUT_DIR=${3:-$(pwd)/output}  # Default output dir
+OUTPUT_DIR=${3:-$(pwd)/output/dts}  # Default output dir
 
 # Check inputs
 if [ -z "$PROJ_FILE" ] || [ -z "$DT_REPO_PATH" ]; then
@@ -22,3 +22,39 @@ hsi::close_hw_design [hsi current_hw_design]
 EOF
 
 echo "Device tree generated in: $OUTPUT_DIR"
+
+# Modify file for SD card
+cd $OUTPUT_DIR
+PCW_FILE="pcw.dtsi"
+if [ ! -f "$PCW_FILE" ]; then
+    echo "Error: $PCW_FILE not found!" >&2
+    exit 1
+fi
+cp "$PCW_FILE" "$PCW_FILE.bak"
+awk '
+/sdhci0/ { in_node=1 }
+in_node && /{/ { brace_count++; print; next }
+in_node && /}/ { 
+    brace_count--;
+    if (brace_count == 0) {
+        print "\tdisable-wp;";
+        print "\tno-1-8-v;";
+        print "\tu-boot,dm-pre-reloc;";
+        in_node=0;
+    }
+}
+in_node { print }
+!in_node { print }
+' "$PCW_FILE.bak" > "$PCW_FILE"
+
+echo "Updated $PCW_FILE successfully. Backup saved as $PCW_FILE.bak"
+
+cat system.dts system-top.dts > temp.dts && \
+    gcc -E -nostdinc -undef -x assembler-with-cpp \
+    -o system-merged.dts \
+    temp.dts
+rm temp.dts
+cp system-merged.dts ../zynqmp.dts
+
+echo "Successfully built dts. Output written to $(pwd)/output/"
+exit 0
