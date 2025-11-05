6df6a1a745bd0c8c6ccf65b0527818ad79f390fb
Emptea <aryas1725@gmail.com>
2025-11-05 10:30:55 +0300

    dts gen logic changed; dtb gen script added

---
+++ scripts/dtb_gen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: ./dtb_gen.sh <linux-xlnx-path> <dts_dir> [output_dir]
+
+LINUX_XLNX_PATH=$1
+DTS_PATH=$2
+OUTPUT_DIR=${3:-$(pwd)/output}  # Default output dir
+
+# Check inputs
+if [ -z "$LINUX_XLNX_PATH" ] || [ -z "$DTS_PATH" ]; then
+    echo "Usage: $0  <linux-xlnx-path> <dts_dir> [output_dir]"
+    exit 1
+fi
+cd $DTS_PATH
+
+INCDIR=$LINUX_XLNX_PATH/include
+DTSHDIR=$LINUX_XLNX_PATH/arch/arm64/boot/dts/
+DTSDIR=$LINUX_XLNX_PATH/arch/arm64/boot/dts/xilinx/
+
+cpp -nostdinc -I${INCDIR} -I${DTSHDIR} -I${DTSDIR} -undef -x assembler-with-cpp -o pre-zynqmp.dts zynqmp.dts
+
+cp pre-zynqmp.dts $OUTPUT_DIR/pre-zynqmp.dts
+cd $OUTPUT_DIR
+echo $(pwd)
+dtc -I dts -O dtb --symbols -i${DTSHDIR} -i${DTSDIR} -o zynqmp.dtb pre-zynqmp.dts
+
+echo "Successfully built dts. Output written to $(pwd)"
+exit 0
--- scripts/dts_gen.sh
+++ scripts/dts_gen.sh
@@ -4,7 +4,8 @@
 
 PROJ_FILE=$1
 DT_REPO_PATH=$2
-OUTPUT_DIR=${3:-$(pwd)/output/dts}  # Default output dir
+OUTPUT_DIR=${3:-$(pwd)/output}  # Default output dir
+OUTPUT_DTS_DIR=$OUTPUT_DIR/dts/
 
 # Check inputs
 if [ -z "$PROJ_FILE" ] || [ -z "$DT_REPO_PATH" ]; then
@@ -17,22 +18,23 @@ xsct <<EOF
 hsi::open_hw_design "$PROJ_FILE"
 hsi::set_repo_path "$DT_REPO_PATH"
 hsi::create_sw_design device-tree -os device_tree -proc psu_cortexa53_0
-hsi::generate_target -dir "$OUTPUT_DIR"
+hsi::generate_target -dir "$OUTPUT_DTS_DIR"
 hsi::close_hw_design [hsi current_hw_design]
 EOF
 
-echo "Device tree generated in: $OUTPUT_DIR"
+echo "Device tree generated in: $OUTPUT_DTS_DIR"
 
 # Modify file for SD card
-cd $OUTPUT_DIR
+cd $OUTPUT_DTS_DIR
 PCW_FILE="pcw.dtsi"
 if [ ! -f "$PCW_FILE" ]; then
     echo "Error: $PCW_FILE not found!" >&2
     exit 1
 fi
 cp "$PCW_FILE" "$PCW_FILE.bak"
+# Modify sdhci1 node
 awk '
-/sdhci0/ { in_node=1 }
+/sdhci1/ { in_node=1 }
 in_node && /{/ { brace_count++; print; next }
 in_node && /}/ { 
     brace_count--;
@@ -45,20 +47,28 @@ in_node && /}/ {
 }
 in_node { print }
 !in_node { print }
-' "$PCW_FILE.bak" > "$PCW_FILE"
+' "$PCW_FILE.bak" > "$PCW_FILE.tmp"
 
+# Modify dwc3_0 node
+awk '
+/&dwc3_0/ { in_node=1 }
+in_node && /{/ { brace_count++; print; next }
+in_node && /}/ { 
+    brace_count--;
+    if (brace_count == 0) {
+        print "\tdr_mode = \"host\";";
+        in_node=0;
+    }
+}
+in_node { print }
+!in_node { print }
+' "$PCW_FILE.tmp" > "$PCW_FILE"
+
+rm "$PCW_FILE.tmp"
 echo "Updated $PCW_FILE successfully. Backup saved as $PCW_FILE.bak"
 
-cat system-top.dts system.dts  > zynqmp.dts && \
-    gcc -E -nostdinc -undef -x assembler-with-cpp \
-    -o system-merged.dts \
-    zynqmp.dts
-rm zynqmp.dts
-cp system-merged.dts ../zynqmp.dts
+cat system-top.dts system.dts pcw.dtsi  > zynqmp.dts
+cp zynqmp.dts ../zynqmp.dts
 
-cd ..
-echo $(pwd)
-dtc -I dts -O dtb -o zynqmp.dtb zynqmp.dts
-
-echo "Successfully built dts. Output written to $(pwd)/output/"
+echo "Successfully built dts. Output written to $OUTPUT_DIR"
 exit 0
