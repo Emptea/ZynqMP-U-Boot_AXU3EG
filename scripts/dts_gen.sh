58765786b0e6221fa8304e9aa936119f081c00c9
Emptea <aryas1725@gmail.com>
2025-11-13 16:28:01 +0300

    dts gen required user.dts for dts user changes

--- scripts/dts_gen.sh
+++ scripts/dts_gen.sh
@@ -23,51 +23,26 @@ hsi::close_hw_design [hsi current_hw_design]
 EOF
 
 echo "Device tree generated in: $OUTPUT_DTS_DIR"
-
-# Modify file for SD card
-cd $OUTPUT_DTS_DIR
-PCW_FILE="pcw.dtsi"
-if [ ! -f "$PCW_FILE" ]; then
-    echo "Error: $PCW_FILE not found!" >&2
-    exit 1
+USER_FILE="user.dts"
+if [ -f "$OUTPUT_DIR/$USER_FILE" ]; then
+    cp $OUTPUT_DIR/$USER_FILE $OUTPUT_DTS_DIR/$USER_FILE
+    echo "Copied $OUTPUT_DIR/$USER_FILE to $OUTPUT_DTS_DIR/$USER_FILE"
+elif [ -f "$(pwd)/$USER_FILE" ]; then
+    cp $(pwd)/$USER_FILE $OUTPUT_DTS_DIR/$USER_FILE
+    echo "Copied $(pwd)/$USER_FILE to $OUTPUT_DTS_DIR/$USER_FILE"
+else
+    echo "Warning: $USER_FILE not found. Creating empty file."
+    touch $OUTPUT_DTS_DIR/$USER_FILE
 fi
-cp "$PCW_FILE" "$PCW_FILE.bak"
-# Modify sdhci1 node
-awk '
-/sdhci1/ { in_node=1 }
-in_node && /{/ { brace_count++; print; next }
-in_node && /}/ { 
-    brace_count--;
-    if (brace_count == 0) {
-        print "\tdisable-wp;";
-        print "\tno-1-8-v;";
-        print "\tu-boot,dm-pre-reloc;";
-        in_node=0;
-    }
-}
-in_node { print }
-!in_node { print }
-' "$PCW_FILE.bak" > "$PCW_FILE.tmp"
 
-# Modify dwc3_0 node
-awk '
-/&dwc3_0/ { in_node=1 }
-in_node && /{/ { brace_count++; print; next }
-in_node && /}/ { 
-    brace_count--;
-    if (brace_count == 0) {
-        print "\tdr_mode = \"host\";";
-        in_node=0;
-    }
-}
-in_node { print }
-!in_node { print }
-' "$PCW_FILE.tmp" > "$PCW_FILE"
+cd $OUTPUT_DTS_DIR
 
-rm "$PCW_FILE.tmp"
-echo "Updated $PCW_FILE successfully. Backup saved as $PCW_FILE.bak"
+if [ -f "$OUTPUT_DTS_DIR/pl.dtsi" ]; then
+    cat system-top.dts system.dts pcw.dtsi pl.dtsi user.dts > zynqmp.dts
+else
+    cat system-top.dts system.dts pcw.dtsi user.dts > zynqmp.dts
+fi 
 
-cat system-top.dts system.dts pcw.dtsi  > zynqmp.dts
 cp zynqmp.dts ../zynqmp.dts
 
 echo "Successfully built dts. Output written to $OUTPUT_DIR"
